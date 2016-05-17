77e1d0b865249e826fd8e949a3e10c46a6614f89
Matthias L. Jugel <leo@thinkberg.com>
2016-05-17 18:31:28 +0200

    add correct SDK

---
+++ SDK_2.0_MK82FN256xxx15/boards/twrk80f150m/driver_examples/sdramc/pin_mux.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_port.h"
+#include "pin_mux.h"
+#include "fsl_common.h"
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Initialize all pins used in this example
+ *
+ * initialization or not.
+ */
+void BOARD_InitPins(void)
+{
+    /* Ungate the port clock */
+    CLOCK_EnableClock(kCLOCK_PortC);
+    /* CLKOUT*/
+    PORT_SetPinMux(PORTC, 3UL, kPORT_MuxAlt5);
+    /* Ungate the port clock */
+    CLOCK_EnableClock(kCLOCK_PortB);
+    /* CAS/RAS*/
+    PORT_SetPinMux(PORTB, 0UL, kPORT_MuxAlt5);
+    PORT_SetPinMux(PORTB, 1UL, kPORT_MuxAlt5);
+    /* WE */
+    PORT_SetPinMux(PORTB, 2UL, kPORT_MuxAlt5);
+    /* CS */
+    PORT_SetPinMux(PORTB, 3UL, kPORT_MuxAlt5);
+    /* SDRAM Data*/
+    PORT_SetPinMux(PORTB, 6UL, kPORT_MuxAlt5);  /* SDRAM_D23*/
+    PORT_SetPinMux(PORTB, 7UL, kPORT_MuxAlt5);  /* SDRAM_D22*/
+    PORT_SetPinMux(PORTB, 8UL, kPORT_MuxAlt5);  /* SDRAM_D21*/
+    PORT_SetPinMux(PORTB, 9UL, kPORT_MuxAlt5);  /* SDRAM_D20*/
+    PORT_SetPinMux(PORTB, 10UL, kPORT_MuxAlt5); /* SDRAM_D19*/
+    PORT_SetPinMux(PORTB, 11UL, kPORT_MuxAlt5); /* SDRAM_D18*/
+    PORT_SetPinMux(PORTB, 16UL, kPORT_MuxAlt5); /* SDRAM_D17*/
+    PORT_SetPinMux(PORTB, 17UL, kPORT_MuxAlt5); /* SDRAM_D16*/
+    PORT_SetPinMux(PORTB, 20UL, kPORT_MuxAlt5); /* SDRAM_D31*/
+    PORT_SetPinMux(PORTB, 21UL, kPORT_MuxAlt5); /* SDRAM_D30*/
+    PORT_SetPinMux(PORTB, 22UL, kPORT_MuxAlt5); /* SDRAM_D29*/
+    PORT_SetPinMux(PORTB, 23UL, kPORT_MuxAlt5); /* SDRAM_D28*/
+    PORT_SetPinMux(PORTC, 12UL, kPORT_MuxAlt5); /* SDRAM_D27*/
+    PORT_SetPinMux(PORTC, 13UL, kPORT_MuxAlt5); /* SDRAM_D26*/
+    PORT_SetPinMux(PORTC, 14UL, kPORT_MuxAlt5); /* SDRAM_D25*/
+    PORT_SetPinMux(PORTC, 15UL, kPORT_MuxAlt5); /* SDRAM_D24*/
+    /* SDRAM Address*/
+    PORT_SetPinMux(PORTC, 0UL, kPORT_MuxAlt5);  /* SDRAM_A22*/
+    PORT_SetPinMux(PORTC, 1UL, kPORT_MuxAlt5);  /* SDRAM_A21*/
+    PORT_SetPinMux(PORTC, 2UL, kPORT_MuxAlt5);  /* SDRAM_A20*/
+    PORT_SetPinMux(PORTC, 4UL, kPORT_MuxAlt5);  /* SDRAM_A19*/
+    PORT_SetPinMux(PORTC, 5UL, kPORT_MuxAlt5);  /* SDRAM_A18*/
+    PORT_SetPinMux(PORTC, 6UL, kPORT_MuxAlt5);  /* SDRAM_A17*/
+    PORT_SetPinMux(PORTC, 7UL, kPORT_MuxAlt5);  /* SDRAM_A16*/
+    PORT_SetPinMux(PORTC, 8UL, kPORT_MuxAlt5);  /* SDRAM_A15*/
+    PORT_SetPinMux(PORTC, 9UL, kPORT_MuxAlt5);  /* SDRAM_A14*/
+    PORT_SetPinMux(PORTC, 10UL, kPORT_MuxAlt5); /* SDRAM_A13*/
+    /* Ungate the port clock */
+    CLOCK_EnableClock(kCLOCK_PortD);
+    PORT_SetPinMux(PORTD, 2UL, kPORT_MuxAlt5); /* SDRAM_A12*/
+    PORT_SetPinMux(PORTD, 3UL, kPORT_MuxAlt5); /* SDRAM_A11*/
+    PORT_SetPinMux(PORTD, 4UL, kPORT_MuxAlt5); /* SDRAM_A10*/
+    PORT_SetPinMux(PORTD, 5UL, kPORT_MuxAlt5); /* SDRAM_A9*/
+    /* SDRAM_CKE*/
+    PORT_SetPinMux(PORTD, 7UL, kPORT_MuxAlt5);
+    /* SDRAM_DQM */
+    PORT_SetPinMux(PORTC, 16UL, kPORT_MuxAlt5); /* SDRAM_DQM2*/
+    PORT_SetPinMux(PORTC, 17UL, kPORT_MuxAlt5); /* SDRAM_DQM3*/
+}
+
+void BOARD_InitPinsForUart(void)
+{
+    /* Affects PORTC_PCR3 register */
+    PORT_SetPinMux(PORTC, 3u, kPORT_MuxAlt3);
+    /* Affects PORTC_PCR4 register */
+    PORT_SetPinMux(PORTC, 4u, kPORT_MuxAlt3);
+}
---
+++ SDK_2.0_MK82FN256xxx15/boards/twrk80f150m/driver_examples/smartcard/pin_mux.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_port.h"
+#include "pin_mux.h"
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Initialize all pins used in this example
+ *
+ * @param disablePortClockAfterInit disable port clock after pin
+ * initialization or not.
+ */
+void BOARD_InitPins(void)
+{
+    /* Ungate the port clock */
+    CLOCK_EnableClock(kCLOCK_PortB);
+    CLOCK_EnableClock(kCLOCK_PortC);
+    CLOCK_EnableClock(kCLOCK_PortA);
+    CLOCK_EnableClock(kCLOCK_PortE);
+    /* Configure external OSC clock */
+    PORT_SetPinMux(PORTA, 18, kPORT_PinDisabledOrAnalog);
+    PORT_SetPinMux(PORTA, 19, kPORT_PinDisabledOrAnalog);
+    /* Configure RTC clock */
+    PORT_SetPinMux(PORTE, 0UL, kPORT_MuxAlt7);
+
+    /* Initialize LPUART1 pins below */
+    /* Ungate the port clock */
+    /* Affects PORTC_PCR3 register */
+    PORT_SetPinMux(PORTC, 3u, kPORT_MuxAlt3);
+    /* Affects PORTC_PCR4 register */
+    PORT_SetPinMux(PORTC, 4u, kPORT_MuxAlt3);
+
+    /* Initialize SMARTCARD pins below */
+    /* IO*/
+    /* Affects PORTB_PCR4 register */
+    port_pin_config_t config = {0};
+    config.openDrainEnable = kPORT_OpenDrainEnable;
+    config.mux = kPORT_MuxAlt2;
+    PORT_SetPinConfig(PORTB, 4u, &config);
+    /* Card clock */
+    config.pullSelect = kPORT_PullUp;
+    config.driveStrength = kPORT_HighDriveStrength;
+    config.mux = kPORT_MuxAlt2;
+    config.openDrainEnable = kPORT_OpenDrainDisable;
+    PORT_SetPinConfig(PORTB, 5u, &config);
+    /* Reset */
+    PORT_SetPinConfig(PORTB, 8u, &config);
+    /* Vsel0 and Vsel1 */
+    config.mux = kPORT_MuxAsGpio;
+    PORT_SetPinConfig(PORTC, 9u, &config);
+    PORT_SetPinConfig(PORTC, 2u, &config);
+    /* Presence Detect */
+    PORT_SetPinMux(PORTB, 7u, kPORT_MuxAlt2);
+    /* INT pin */
+    config.mux = kPORT_MuxAsGpio;
+    PORT_SetPinConfig(PORTA, 13u, &config);
+    /* Initialize pin interrupt */
+    PORT_SetPinInterruptConfig(PORTA, 13u, kPORT_InterruptEitherEdge);
+    /* Clear interrupt status flags */
+    PORT_ClearPinsInterruptFlags(PORTA, 1u << 13u);
+    /* VCCEN */
+    config.mux = kPORT_MuxAlt2;
+    PORT_SetPinConfig(PORTB, 6u, &config);
+}
