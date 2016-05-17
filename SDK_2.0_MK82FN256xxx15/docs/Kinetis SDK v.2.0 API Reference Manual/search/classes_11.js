f82560025e3f193587d6960b100d74499e4b5098
Matthias L. Jugel <leo@thinkberg.com>
2016-05-17 18:30:26 +0200

    deleted wrong SDK

--- SDK_2.0_MK82FN256xxx15/docs/Kinetis SDK v.2.0 API Reference Manual/search/classes_11.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['uart_5fconfig_5ft',['uart_config_t',['../group__uart__driver.html#structuart__config__t',1,'']]],
-  ['uart_5frtos_5fhandle_5ft',['uart_rtos_handle_t',['../group__uart__freertos__driver.html#structuart__rtos__handle__t',1,'']]],
-  ['uart_5ftransfer_5ft',['uart_transfer_t',['../group__uart__driver.html#structuart__transfer__t',1,'']]]
-];
--- SDK_2.0_MK82FN256xxx15/docs/Kinetis SDK v.2.0 API Reference Manual/search/classes_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['gpio_5fpin_5fconfig_5ft',['gpio_pin_config_t',['../group__gpio.html#structgpio__pin__config__t',1,'']]]
-];
